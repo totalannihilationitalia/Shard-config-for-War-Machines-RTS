cfdd364cfeec7e59a1f00edfeca2f6556d8b872c
totalannihilationitalia <totalannihilationitalia@gmail.com>
2025-02-04 20:57:16 +0100

    Initial commit

---
+++ dev/ai/attackers.lua
@@ -0,0 +1,26 @@
+--[[
+Attackers!
+]]--
+
+attackerlist = {
+-- NFA side
+
+"corfav",
+"corgator",
+"corgarp",
+"corraid",
+"corlevlr",
+"corwolv",
+"cormist",
+"corak",
+"corstorm",
+"corthud",
+"corcrash",
+"corstorm",
+
+-- AND side
+"anddauber",
+"andgaso",
+"andlipo",
+"andmisa",
+}
